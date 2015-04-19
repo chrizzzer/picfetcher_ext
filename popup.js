d6aa8ff414f27c2673faaef240ce7c905afd1a73
Christian Haug <chris-haug@gmx.de>
2015-04-18 22:13:30 -0400

    url fetch and display in extension now working

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
 function getCurrentUrl(callback) {
 	var queryInfo = {
 		active: true,
-		currentWindow:true
-	}
+		currentWindow: true
+	};
 
 	chrome.tabs.query(queryInfo, function(tabs){
-		var tab = tabs.[0];
+		var tab = tabs[0];
 		var url = tab.url;
-		callback(url);
-	})
 
-	document.getElementById('status').textContent = url;
+		callback(url);
+	});
+
 }
+
+function renderStatus(statusText) {
+  document.getElementById('currenturl').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() { 
+	getCurrentUrl( 
+			function(url){
+				renderStatus('The current url is: ' + url);
+			}
+		);
+	
+	}
+); 
